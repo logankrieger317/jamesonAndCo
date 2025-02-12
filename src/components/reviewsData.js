4a038fce5141c497dc73c6a9c0af67c5e3e16edb
Logan <logankrieger317@gmail.com>
2025-02-11 20:52:54 -0600

    reviews

--- src/components/reviewsData.js
+++ src/components/reviewsData.js
@@ -3,61 +3,193 @@ import DefaultReviewIcon from "../Images/defaultIcon";
 const reviews = [
   {
     id: 1,
+    text: "Excellent grooming service for my Shih Tzu Bianca. Very satisfied with the results!",
+    author: "David Z.",
+    date: "Feb 9, 2025",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 2,
+    text: "Great experience with my Shih Tzu Motor. Hailey did an amazing job!",
+    author: "Henry R.",
+    date: "Feb 9, 2025",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 3,
+    text: "My Labradoodle Reese looks fantastic! Professional and caring service.",
+    author: "Reagan S.",
+    date: "Feb 9, 2025",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 4,
+    text: "Charlie, my Goldendoodle, received excellent care. Very happy with the grooming!",
+    author: "Kelly P.",
+    date: "Feb 9, 2025",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 5,
+    text: "Joey, our Labradoodle, looks amazing after his grooming session. Highly recommend!",
+    author: "Laurie P.",
+    date: "Feb 9, 2025",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 6,
+    text: "Royal, my Golden Retriever, was treated with great care. Excellent service!",
+    author: "Anthony H.",
+    date: "Feb 8, 2025",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 7,
+    text: "Very professional handling of my German Shepherd, Eloy. Great experience!",
+    author: "Paul N.",
+    date: "Feb 8, 2025",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 8,
+    text: "Reggie (Rumi), my Standard Poodle, received excellent grooming. Very satisfied!",
+    author: "Jane A.",
+    date: "Feb 5, 2025",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 9,
+    text: "Pepo, my Pug, looks fantastic after his grooming session. Great service!",
+    author: "Miguel S.",
+    date: "Feb 5, 2025",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 10,
+    text: "Bella, our Chihuahua, was treated with such care. Wonderful experience!",
+    author: "Pat M.",
+    date: "Feb 5, 2025",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 11,
+    text: "ENZO, my Aussiedoodle, received excellent grooming. Very happy with the results!",
+    author: "Katrina T.",
+    date: "Feb 4, 2025",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 12,
+    text: "Both Ranger and Blake, my Australian Shepherds, look amazing. Great job!",
+    author: "Steven M.",
+    date: "Feb 2, 2025",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 13,
+    text: "Bexar (bear), my German Shepherd, was groomed perfectly. Excellent service!",
+    author: "Samuel W.",
+    date: "Feb 2, 2025",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 14,
+    text: "Mavella, my Mini poodle mix, received wonderful care. Very professional!",
+    author: "Rita Z.",
+    date: "Mar 23, 2024",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 15,
+    text: "Winnie, our Great Pyrenees, looks beautiful after grooming. Highly recommend!",
+    author: "Holly S.",
+    date: "Mar 22, 2024",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 16,
     text: "Hailey has the magic touch with my quirky, little rescue dog. She was able to give him the full bath treatment, including nails, teeth and anal glands. I was amazed! She also fit my smelly dog in very quickly for his much needed cleaning. I highly recommend her.",
     author: "Eileen C.",
     date: "Sep 19, 2023",
     image: "https://s3-media0.fl.yelpcdn.com/photo/6BkSS0lRjHQr7YPGBZd9Bg/60s.jpg"
   },
   {
-    id: 2,
+    id: 17,
     text: "Jameson and Company Dog Grooming comes highly recommended for their exceptional service. They are particularly praised for their expertise in grooming fluffy doodle/poodle-like haired pups, including Portuguese Water Dogs and Australian Shepherds. The groomers at Jameson and Company are known for their caring approach towards dogs, ensuring a comfortable and friendly environment for your pet. Whether your dog needs a simple bath or a full haircut/shave, this trendy grooming salon is equipped to meet your needs. Despite their popularity, they are currently accepting new clients, making it an ideal choice for those who have recently moved to the area or are in search of a reliable groomer.",
     author: "nextdoor App",
     date: "2023 Neighborhood Fave",
     image: "https://d19rpgkrjeba2z.cloudfront.net/b4ffc5499aaf214a/static/images/awards/neighborhood_fave_2023.svg"
-  },{
-
-  id:3,
-  text:"Hailey is always a delight to take my dog to. She does great work and cares deeply.",
-  author:"Carrie H",
-  date:"March 2024",
-  image: DefaultReviewIcon()
   },
   {
-    id:4,
-    text:"She is very prompt and my dog loves going there and she is not easy to deal with She is very stubborn !",
-    author:"Louise M.",
-    date:"Nov 1, 2023",
+    id: 18,
+    text: "Hailey is always a delight to take my dog to. She does great work and cares deeply.",
+    author: "Carrie H",
+    date: "March 2024",
     image: DefaultReviewIcon()
   },
   {
-    id:5,
-    text:"1st time here and it is now on my forever list for getting both pups groomed. Fabulous !!!!",
-    author:"Terry J.",
-    date:"Dec 12, 2024",
+    id: 19,
+    text: "She is very prompt and my dog loves going there and she is not easy to deal with She is very stubborn!",
+    author: "Louise M.",
+    date: "Nov 1, 2023",
     image: DefaultReviewIcon()
   },
   {
-    id:6,
-    text:"I have been using Hailey as my groomer for a few years now and she can get appointments faster than most groomers. Her attention to detail is amazing! My Aussie has the best groomer in all of Austin. Trust me on this!",
-    author:"Larrette",
-    date:"Oct 13, 2022",
+    id: 20,
+    text: "1st time here and it is now on my forever list for getting both pups groomed. Fabulous!!!!",
+    author: "Terry J.",
+    date: "Dec 12, 2024",
     image: DefaultReviewIcon()
   },
   {
-    id:7,
-    text:"I cannot rave about Hailey. She is an absolute GEM of a human. She is so kind, and truly cares about every one of her clients. She goes above and beyond to get to know the pups and their parents. Our Weimardoodle was just experiencing bad grooming luck until we met her. Our lab doesn’t think twice about going right to her, even for nail trims and grinding. Our pups adore her and so do we!",
-    author:"April S.",
-    date:"Feb 9, 2023",
+    id: 21,
+    text: "I have been using Hailey as my groomer for a few years now and she can get appointments faster than most groomers. Her attention to detail is amazing! My Aussie has the best groomer in all of Austin. Trust me on this!",
+    author: "Larrette",
+    date: "Oct 13, 2022",
     image: DefaultReviewIcon()
   },
   {
-    id:8,
-    text:"Would highly recommend this groomer. First time today and our dog is groomed to perfection. Will definitely return!",
-    author:"Holly S.",
-    date:"Mar 15, 2023",
+    id: 22,
+    text: "Hailey is always a delight to take my dog to. She does great work and cares deeply.",
+    author: "Carrie H",
+    date: "March 2024",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 23,
+    text: "She is very prompt and my dog loves going there and she is not easy to deal with She is very stubborn !",
+    author: "Louise M.",
+    date: "Nov 1, 2023",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 24,
+    text: "1st time here and it is now on my forever list for getting both pups groomed. Fabulous !!!!",
+    author: "Terry J.",
+    date: "Dec 12, 2024",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 25,
+    text: "I have been using Hailey as my groomer for a few years now and she can get appointments faster than most groomers. Her attention to detail is amazing! My Aussie has the best groomer in all of Austin. Trust me on this!",
+    author: "Larrette",
+    date: "Oct 13, 2022",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 26,
+    text: "I cannot rave about Hailey. She is an absolute GEM of a human. She is so kind, and truly cares about every one of her clients. She goes above and beyond to get to know the pups and their parents. Our Weimardoodle was just experiencing bad grooming luck until we met her. Our lab doesn’t think twice about going right to her, even for nail trims and grinding. Our pups adore her and so do we!",
+    author: "April S.",
+    date: "Feb 9, 2023",
+    image: DefaultReviewIcon()
+  },
+  {
+    id: 27,
+    text: "Would highly recommend this groomer. First time today and our dog is groomed to perfection. Will definitely return!",
+    author: "Holly S.",
+    date: "Mar 15, 2023",
     image: DefaultReviewIcon()
   }
-
 ];
 
 export default reviews;
